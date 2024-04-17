63e5dde77351be1d2244ea25282aa9322da82200
Jordi <jordi.be.1992@gmail.com>
2024-04-17 17:25:54 +0200

    Added round to the icons on the header + section text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ import ProjectsIcon from "./Projects";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center py-3 mx-5">
-      <main className="flex gap-x-4">
+    <header className="flex justify-evenly items-center pt-5">
+      <main className="flex gap-x-3">
         <img
           className="rounded-none w-[50px] h-[50px]"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjOFdoclVsGjqT0YHiCwwGLNjor88ki6dhNjZVrjmmHg&s"
@@ -15,11 +15,11 @@ export default function Header() {
         />
         <div className="flex flex-col">
           <span className="text-2xl font-bold">Nombre Apellido</span>
-          <span>Web developer</span>
+          <span className="opacity-80">Web developer</span>
         </div>
       </main>
 
-      <nav className="flex flex-row gap-x-5">
+      <nav className="flex gap-x-5">
         <IconRedirect href="https://github.com">
           <GithubIcon />
         </IconRedirect>
--- src/components/IconRedirect.jsx
+++ src/components/IconRedirect.jsx
@@ -1,8 +1,10 @@
-export default function IconRedirect({ href, target = "_blank", children }) {
+export default function IconRedirect({ href, target = "_blank", rel = "noopener noreferrer", children }) {
   return (
     <a
+      className="rounded-full flex items-center justify-center aspect-square shadow-xl dark:shadow-thick hover:bg-zinc-800 bg-primary-950 dark:ring-white/30 ring-primary/20 dark:bg-red-100/10 hover:ring-primary/5 dark:hover:ring-white/20 duration-300 transition-all h-12 w-12"
       href={href}
-      target={target}>
+      target={target}
+      rel={rel}>
       {children}
     </a>
   );
