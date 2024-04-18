50813f4c44b8541bf9a241858d32b17d0d2fab46
Jggcod <jggcod@gmail.com>
2024-04-18 17:00:48 +0200

    Second commit fixing problems with cards and layaout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,18 @@
 function Card({ title, description, imgPath, alt }) {
   return (
-    <div className="border-2 border-gray-700 border-zinc-900 ">
-      <img src={imgPath} alt={alt} />
-      <h1>{title}</h1>
-      <h2>{description}</h2>
+    <div className="border-2 border-gray-700 border-zinc-900  ">
+      <div className="max-w-full">
+        <img className="object-contain h-48 w-96" src={imgPath} alt={alt} />
+      </div>
+      <div className="py-5 px-5">
+        <h1 className="text-2xl font-bold underline text-balance ">{title}</h1>
+        <h2 className="flex flex-wrap text-justify opacity-80 py-2 text-pretty ">
+          {description}
+        </h2>
+        <button className="font-medium text-sm px-4 py-2  font-medium text-center text-white bg-white-700 hover:bg-white-800 focus:ring-4 focus:outline-none focus:ring-white-300 dark:bg-white-600 dark:hover:bg-white-700 dark:focus:ring-white-800 bg-zinc-800">
+          Access
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,18 +6,18 @@ import ProjectsIcon from "./Projects";
 
 export default function Header() {
   return (
-    <header className="flex justify-evenly items-center pt-5">
-      <main className="flex gap-x-3">
+    <header className="flex justify-between items-center pt-5 container">
+      <div className="flex gap-x-3">
         <img
           className="rounded-none w-[50px] h-[50px]"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjOFdoclVsGjqT0YHiCwwGLNjor88ki6dhNjZVrjmmHg&s"
           alt="Photo"
         />
         <div className="flex flex-col">
-          <span className="text-2xl font-bold">Nombre Apellido</span>
+          <span className="text-2xl font-bold uppercase">Nombre Apellido</span>
           <span className="opacity-80">Web developer</span>
         </div>
-      </main>
+      </div>
 
       <nav className="flex gap-x-5">
         <IconRedirect href="https://github.com">
